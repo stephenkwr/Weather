a336852c19e533e7ada520539f6910818f38ead6
stephenkwr <95675560+stephenkwr@users.noreply.github.com>
2025-11-30 22:33:19 +0800

    Fixed some errors
    spelling error of final import

--- Custom_modules/telegram_bot.py
+++ Custom_modules/telegram_bot.py
@@ -1,4 +1,4 @@
-from typing import Optional, final
+from typing import Optional, Final
 from telegram import Update, Bot
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
